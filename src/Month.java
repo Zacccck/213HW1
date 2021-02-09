4f59ea7c51177df4eab830397c35752fa70bbad9
alexgalvan <alexgalvan0630@yahoo.com>
2021-02-08 22:25:46 -0500

    Add files via upload

---
+++ src/Month.java
@@ -0,0 +1,24 @@
+public class Month {
+	
+	public static final int JANUARY = 1;
+    public static final int FEBRUARY = 2;
+    public static final int MARCH = 3;
+    public static final int APRIL = 4;
+    public static final int MAY = 5;
+    public static final int JUNE = 6;
+    public static final int JULY = 7;
+    public static final int AUGUST = 8;
+    public static final int SEPTEMBER = 9;
+    public static final int OCTOBER = 10;
+    public static final int NOVEMBER = 11;
+    public static final int DECEMBER = 12;
+    public static final int BEFORE1900 = 1900;
+    public static final int MONTHS_FIRST_DAY = 1;
+    public static final int MONTHS_31DAYS = 31;
+    public static final int MONTHS_30DAYS = 30;
+    public static final int LEAPYEAR = 29;
+    public static final int NORMALYEAR = 28;
+    public static final int QUADRENNIAL = 4;
+    public static final int CENTENNIAL = 100;
+    public static final int QUATERCENTENNIAL = 400;
+}
