f939c7df3f31bc88e337a6ebbc21815beccabdc4
alexgalvan <alexgalvan0630@yahoo.com>
2021-02-09 22:40:21 -0500

    Update Kiosk.java

--- src/Kiosk.java
+++ src/Kiosk.java
@@ -1,158 +1,129 @@
 /**
- * Properties of a Date.
- * Includes constructors, getter methods, and isValid to check for valid dates.
+ * Command center for handling commands given at the command line.
  * @author Alexander Galvan, Yuan Zhao
  */
 package src;
-import java.util.*;
+import java.util.Scanner;
 
-public class Date {
-    private int year;
-    private int month;
-    private int day;
-    
-    public static final int JANUARY = 1;
-    public static final int FEBRUARY = 2;
-    public static final int MARCH = 3;
-    public static final int APRIL = 4;
-    public static final int MAY = 5;
-    public static final int JUNE = 6;
-    public static final int JULY = 7;
-    public static final int AUGUST = 8;
-    public static final int SEPTEMBER = 9;
-    public static final int OCTOBER = 10;
-    public static final int NOVEMBER = 11;
-    public static final int DECEMBER = 12;
-    public static final int BEFORE1900 = 1900;
-    public static final int MONTHS_FIRST_DAY = 1;
-    public static final int MONTHS_31DAYS = 31;
-    public static final int MONTHS_30DAYS = 30;
-    public static final int LEAPYEAR = 29;
-    public static final int NORMALYEAR = 28;
-    public static final int QUADRENNIAL = 4;
-    public static final int CENTENNIAL = 100;
-    public static final int QUATERCENTENNIAL = 400;
+public class Kiosk{
+	private String command;
 
-    //taking mm/dd/yyyy and create a Date object
-    public Date(String d){
-        String dates[] = d.split("/");
-		month = Integer.parseInt(dates[0]);
-		day = Integer.parseInt(dates[1]);
-		year = Integer.parseInt(dates[2]);
-	}
-
-    //creating an object with today's date (see Calendar class)
-    public Date(){
-        Calendar cal = Calendar.getInstance();
-
-        year = cal.get(Calendar.YEAR);
-        month = cal.get(Calendar.MONTH);
-        day = cal.get(Calendar.DATE);
-    }
-    
-    public int getYear(){
-		return this.year;
-	}
+	public static final int A_ARGS = 3;
+	public static final int ROI_ARGS = 2;
+	public static final int P_ARGS = 1;
 	
-	public int getMonth(){
-		return this.month;
-	}
-	
-	public int getDay(){
-		return this.day;
-	}
-
-    public boolean isValid(){
-        Calendar currentDate = Calendar.getInstance();
-        Calendar thisDate = Calendar.getInstance();
-            thisDate.set(year, month, day);
-
-        
-        if(year < BEFORE1900){
-            return false;
-        }
-
-        if(thisDate.after(currentDate)){
-            return false;
-        }
-
-        if (month > DECEMBER || month < JANUARY) {
-			return false;
-		}
-		if (day > MONTHS_31DAYS || day < MONTHS_FIRST_DAY) {
-			return false;
-		}
-		// ensures for months April,June,September, and November do not have more than
-		// 30 days
-		if ((month == APRIL || month == JUNE || month == SEPTEMBER || month == NOVEMBER) && day > MONTHS_30DAYS) {
-			return false;
-		}
-		// ensures Feburary has no more than 28 days on non leapyears, and 29 on
-		// leapyears
-		if ((month == FEBRUARY && !leapYear(year) && day > NORMALYEAR) || (month == FEBRUARY && leapYear(year) && day > LEAPYEAR)) {
-			return false;
-		}
-
-		return true;
-        
-    }
-
-    // User created method, checks if there is a leap year
-	private static boolean leapYear(int yr) {
-		if (yr % QUADRENNIAL == 0) {
-			if (yr % CENTENNIAL == 0) {
-				if (yr % QUATERCENTENNIAL == 0) {
-					return true;
-				}
-				return false;
-			}
-			return true;
-		}
-		return false;
-	}
-	
-	@Override
-	public String toString() {
-		return month + "/" + day + "/" + year;
-	}
-
-
-
-	public static void main(String[] args) {
-		Date currentDate = new Date(); //test case for default constructor
-		Date validDate = new Date("03/01/2003"); //valid test case using constructor  
-		Date before1900 = new Date("01/11/1899"); //invalid test case; year is before 1900
-		Date afterCurrentYear = new Date("01/11/2030"); //invalid test case; year is after current date
-		Date afterCurrentMonth = new Date("03/11/2021");
-		Date validLeapyear = new Date("02/29/2000"); //valid test case; year 2000 was a leapyear
-		Date invalidLeapYear = new Date("02/29/2019"); //invalid test case; year 2019 was not a leapyear
-		Date invalidMonth = new Date("13/03/2020");
-		Date invalidMonthDay = new Date("12/32/2020");
-		Date invalid_30_DayMonth = new Date("04/31/2020");
-		Date invalid_31_DayMonth = new Date("03/32/2020");
+	/**
+	 * Takes commands from the user, checks if they are valid, and processes them.
+	 * Terminates once Q is input.
+	 * @return void
+	 */
+	public void run() {
+		System.out.println("Library Kiosk running.");
 		
-		if(currentDate.isValid())
-			System.out.println("currentDate is valid!");
-		if(validDate.isValid())
-			System.out.println("validDate is valid!");
-		if(!before1900.isValid())
-			System.out.println("before1900 is invalid!");
-		if(!afterCurrentYear.isValid())
-			System.out.println("afterCurrentYear is invalid!");
-		if(!afterCurrentMonth.isValid())
-			System.out.println("afterCurrentMonth is invalid!");
-		if(validLeapyear.isValid())
-			System.out.println("validLeapyear is valid!");
-		if(!invalidLeapYear.isValid())
-			System.out.println("invalidLeapyear is invalid!");
-		if(!invalidMonth.isValid())
-			System.out.println("invalidMonth is invalid!");
-		if(!invalidMonthDay.isValid())
-			System.out.println("invalidMonthDay is invalid!");
-		if(!invalid_30_DayMonth.isValid())
-			System.out.println("invalid_30_DayMonth is invalid!");
-		if(!invalid_31_DayMonth.isValid())
-			System.out.println("invalid_31_DayMonth is invalid!");
+		Library lib = new Library();
+		
+		Scanner in = new Scanner(System.in);
+		
+		do{
+			String input = in.nextLine();
+			String tokens[] = input.split(",");
+			
+			if((tokens.length != A_ARGS) && (tokens.length != ROI_ARGS) && (tokens.length != P_ARGS))
+				System.out.println("Invalid Command");
+				
+			command = tokens[0];
+			
+			switch(command) {
+				case "A":
+						if(tokens.length != A_ARGS) {
+							System.out.println("Invalid Command");
+							break;
+						}
+						Date date = new Date(tokens[2]);
+						if(date.isValid()) {
+							Book book = new Book(tokens[1], date);
+							lib.add(book);
+							System.out.println(tokens[1] + " added to the Library.");
+						}
+						else
+							System.out.println("Invalid date!");
+						break;
+				case "R":
+						if(tokens.length != ROI_ARGS) {
+							System.out.println("Invalid Command");
+							break;
+						}
+						Book book = new Book(tokens[1]);
+						if(lib.remove(book))
+							System.out.println("Book# " + tokens[1] + " removed.");
+						else
+							System.out.println("Unable to remove, the library does not have this book.");
+						break;
+				case "O":
+						if(tokens.length != ROI_ARGS) {
+							System.out.println("Invalid Command");
+							break;
+						}
+						Book book1 = new Book(tokens[1]);
+						if(lib.checkOut(book1))
+							System.out.println("You've checked out Book#" + tokens[1] + ". Enjoy!");
+						else
+							System.out.println("Book#" + tokens[1] + " is not available.");
+						break;
+				case "I":
+						if(tokens.length != ROI_ARGS) {
+							System.out.println("Invalid Command");
+							break;
+						}
+						Book book2 = new Book(tokens[1]);
+						if(lib.returns(book2)) {
+							System.out.println("Book#" + tokens[1] + " has completed. Thanks!");
+						}
+						else
+							System.out.println(" Unable to return Book#" + tokens[1] + ".");
+						break;
+				case "PA":
+						if(tokens.length != P_ARGS) {
+							System.out.println("Invalid Command");
+							break;
+						}
+						if(lib.isEmpty())
+							System.out.println("Library catalog is empty!.");
+						else
+							lib.print();
+						break;
+				case "PD":
+						if(tokens.length != P_ARGS) {
+							System.out.println("Invalid Command");
+							break;
+						}
+						if(lib.isEmpty())
+							System.out.println("Library catalog is empty!.");
+						else
+							lib.printByDate();
+						break;
+				case "PN":
+						if(tokens.length != P_ARGS) {
+							System.out.println("Invalid Command");
+							break;
+						}
+						if(lib.isEmpty())
+							System.out.println("Library catalog is empty!.");
+						else
+							lib.printByNumber();
+						break;
+				case "Q":
+						System.out.println("Kiosk session ended.");
+						break;
+				case "":
+						break;
+				default:
+						System.out.println("Invalid Command!");
+						break;
+			}
+			
+		}
+		while(command != "Q");
 		
 	}
 }
